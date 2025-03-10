7284bf9b2c46c3ff7585bffa0746e333c8319cfc
Lithira-Sasmitha <lithirasasmitha@gmail.com>
2025-03-10 23:04:38 +0530

    slide bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1 className='bg-blue-400 '> ITP PROJECT</h1>
-    </div>
+    <h1 className="text-3xl font-bold underline font-quickSand">Hello world!</h1>
+    
+/* <div><h1 className='bg-blue-400 '> ITP PROJECT</h1></div> */
+    
   );
 }
 
