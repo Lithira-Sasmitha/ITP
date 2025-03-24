03bb25239c8930d06ecae25eb1f8b2661faef07c
Lithira-Sasmitha <lithirasasmitha@gmail.com>
2025-03-25 03:13:41 +0530

    port change

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.get('/api/test', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5055;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- frontend/src/store/apiSLice.js
+++ frontend/src/store/apiSLice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseURL = 'http://localhost:5055';
+const baseURL = 'http://localhost:5000';
 
 
 export const apiSlice = createApi({
